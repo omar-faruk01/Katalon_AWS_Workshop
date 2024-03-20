cbd2e5d1d411efb09e5242872774fa88c2066efb
gia <gia@192.168.4.26>
2024-03-20 13:29:14 -0400

    update test suite

--- Test Suites/Cura Healthcare.ts
+++ Test Suites/Cura Healthcare.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>dd9b8724-2db5-42d7-b912-cba8e8aa5aa6</testSuiteGuid>
+   <testCaseLink>
+      <guid>5865659e-149c-49f5-8553-8b3f483c07d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Book Appointment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
